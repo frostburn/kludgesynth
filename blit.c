800fc00c5c7811454ee147cc59f873de0285afbc
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2016-05-31 22:00:18 +0300

    Implement cheaper blits.

--- blit.c
+++ blit.c
@@ -1,8 +1,32 @@
+#include <complex.h>
+
 typedef struct blit_state
 {
     double phase;
     double freq;
 } blit_state;
+
+double sine_series_mu(double phase, double n)
+{
+    n += 1;
+    double floor_n = floor(n);
+    double mu = n - floor_n;
+    floor_n *= phase;
+    double s = sine(0.5 * floor_n);
+    return -tan(M_PI * (phase + 0.5)) * s * s + sine(floor_n) * (mu - 0.5);
+}
+
+double sine_series_odd_mu(double phase, double n)
+{
+    double s = sine(phase);
+    if (fabs(s) < EPSILON) {
+        return s * n * n;
+    }
+    double floor_n = floor(n);
+    double mu = n - floor_n;
+    floor_n *= 2 * phase;
+    return (0.5 - 0.5 * cosine(floor_n)) / s + sine(floor_n + phase) * mu;
+}
 
 // Returns sum(z ** i for i in range(n)) with linear fading of the last power.
 complex double power_series_mu(complex double z, double n)
--- generate.c
+++ generate.c
@@ -1,25 +1,31 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAIN
 #include "util.c"
+#include "waveform.c"
 
 #define SAMPLE_RATE (44100)
 #define SAMPDELTA (1.0 / (double) SAMPLE_RATE)
-#define NUM_SAMPLES (SAMPLE_RATE * 3)
+#define NUM_SAMPLES (SAMPLE_RATE * 5)
 
+#include "blit.c"
 #include "filter.c"
 
 int main()
 {
+    waveform_init();
     double *samples = malloc(NUM_SAMPLES * sizeof(double));
 
     filter_state filter;
 
     double max_amplitude = 0;
     for (int i = 0; i < NUM_SAMPLES; i++) {
-        filter_lpf(&filter, i * SAMPDELTA * 1000 + 100, 1000);
-        samples[i] = filter_step(&filter, frand() * 0.1);
+        // filter_lpf(&filter, i * SAMPDELTA * 1000 + 100, 1000);
+        // samples[i] = filter_step(&filter, frand() * 0.1);
+
+        samples[i] = sine_series_odd_mu(i * 0.01, 0.0 + 3 * i * SAMPDELTA) * 0.1;
 
         if (i < 20) {
             printf("%g\n", samples[i]);
